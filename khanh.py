5413687cd81c3c2194890762d2c91d018b135102
DESKTOP-49PPGNM\Lenovo <chtkkiet@gmail.com>
2023-07-06 20:54:30 +0700

    Them buoi2

--- khanh.py
+++ khanh.py
@@ -4,3 +4,5 @@ print("I am {age}")
 
 # Git: quản lý version và code
 # git clone: tải code từ github
+# git add .: thêm tất cả file vào danh sách muốn lưu trên git
+# git commit -m "ABC": "ABC" là tên / note của phiên bản code tại lúc đó
